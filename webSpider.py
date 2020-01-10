0b1dcc1528092fafce07185d3a442030c4c1e04e
SamCheung <samsui@hotmail.com>
2020-01-10 13:14:50 -0500

    parameterization of spider

--- webSpider.py
+++ webSpider.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import sys
+
+
 #import pandas as pd
 """
   Developer   : Sam Cheung
@@ -12,11 +15,10 @@ from bs4 import BeautifulSoup
                 Pandas(only required for analysis)             
 """
 
-
 # Initial block #
-baseurl = "https://wiprodigital.com"
-URL_4_Visits = [baseurl]
-level = 1
+#baseurl = "https://wiprodigital.com"
+#URL_4_Visits = [baseurl]
+#level = 1
 
 # exclusion list of sites
 exclude_list = ["facebook.com", "twitter.com", "linkedin.com", "google.com", "youtube.com",
@@ -37,26 +39,35 @@ def getAndParseURL(current_url):
         return url_list
 
 # Main loop until all pages are visited
-while len(URL_4_Visits) != 0:
-    for visiting in URL_4_Visits:
-        links = []
-        df = {}
-        for link in set(getAndParseURL(visiting)):
-            url = link['href']
-            if not any(x in url for x in exclude_list) and not any(url in v for v in visited) and url.startswith('http'):
-               links.append(url)
-               print(" visited : " + url)
 
-            else:
-               print(" skipped : " + url)
-            visited.append(url)
-    # df["level" + str(level)] = pd.DataFrame(set(links), columns=['url'])
-    level += 1
-    URL_4_Visits = links
-    if len(URL_4_Visits) == 0:
-        print("\n\n=====  visited pages ({}) =====".format(str(len(set(visited)))))
-        for out in set(visited):
-            if (out != 'x' or 'mailto' not in out) and out.startswith('http'):
-               print(out)
-    else:
-        print("process level" + str(level))
+
+def main():
+    level = 1
+    URL_4_Visits = [str(sys.argv[1])]
+    while len(URL_4_Visits) != 0:
+        for visiting in URL_4_Visits:
+            links = []
+            df = {}
+            for link in set(getAndParseURL(visiting)):
+                url = link['href']
+                if not any(x in url for x in exclude_list) and not any(url in v for v in visited) and url.startswith('http'):
+                   links.append(url)
+                   print(" visited : " + url)
+
+                else:
+                   print(" skipped : " + url)
+                visited.append(url)
+        # df["level" + str(level)] = pd.DataFrame(set(links), columns=['url'])
+        level += 1
+        URL_4_Visits = links
+        if len(URL_4_Visits) == 0:
+            print("\n\n=====  visited pages ({}) =====".format(str(len(set(visited)))))
+            for out in set(visited):
+                if (out != 'x' or 'mailto' not in out) and out.startswith('http'):
+                   print(out)
+        else:
+            print("process level" + str(level))
+
+
+if __name__ == '__main__':
+    main()
